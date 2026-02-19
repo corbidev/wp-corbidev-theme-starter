9f8b7dcf8a9bf4e84cc24db6fc5b44ebac43bc2e
Eric Corbisier <contact@corbisier.fr>
2026-02-19 13:30:27 +0100

    Correction vite et tailwind et nettoyage v2

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,23 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [vue()],
   build: {
     outDir: 'assets/dist',
     emptyOutDir: true,
     manifest: true,
+
+    // 🔥 Toujours générer les maps
+    sourcemap: true,
+
+    // 🔥 Minifier uniquement en prod
+    minify: mode === 'production',
+
     rollupOptions: {
       input: {
         app: path.resolve(__dirname, 'assets/src/main.js'),
       }
     }
   }
-})
+}))
